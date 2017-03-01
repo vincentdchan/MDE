6a9aaed75aed7a506343fabff44b35d9fcbd1036
ChannelOne <cdzos97@gmail.com>
2017-03-01 21:07:46 +0800

    fix showNumber option bug when boot

--- src/controller/configuration.ts
+++ src/controller/configuration.ts
@@ -33,6 +33,7 @@ export function configurationThunk(mde: MDE) : Config {
                         }
                     },
                     triggerOnStart: true,
+                    value: true,
                 }
             }
         }, 
